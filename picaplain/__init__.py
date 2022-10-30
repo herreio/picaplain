a7a714e94d34c79fde09f21d0de54f4188df895c
Donatus Herre <donatus.herre@slub-dresden.de>
2022-10-30 02:37:28 +0200

    Bump version number

--- picaplain/__init__.py
+++ picaplain/__init__.py
@@ -3,6 +3,6 @@ This package allows to parse plain serialized PICA data from K10plus.
 """
 
 __author__ = "Donatus Herre <donatus.herre@slub-dresden.de>"
-__version__ = "0.0.0"
+__version__ = "0.0.1"
 
 from .parser import K10plusTitle as K10plus
